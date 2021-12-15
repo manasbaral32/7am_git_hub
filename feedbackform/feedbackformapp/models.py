527fc22a4cbc7b196ece4cc8cf2e81fd4ec33fd2
manasbaral32 <96184692+manasbaral32@users.noreply.github.com>
2021-12-15 21:14:46 +0530

    Update models.py

--- feedbackform/feedbackformapp/models.py
+++ feedbackform/feedbackformapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+#This is a modal
+
 class FeedbackData(models.Model):
     name = models.CharField(max_length=30)
     datetime = models.DateTimeField(auto_now_add=True)
